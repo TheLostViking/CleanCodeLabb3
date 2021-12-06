6f2da1295d224355028e74331d1f9bc2806b5ede
Dan Mehlqvist <dan.mehlqvist@gmail.com>
2021-12-06 19:40:24 +0100

    Add project files.

---
+++ HouseF.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace HouseF.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
